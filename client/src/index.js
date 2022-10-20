5cc92024dc6d7b1daf5f05d81f0f30286ea797f8
aurimas12 <aurimas.butk@gmail.com>
2022-10-20 14:38:32 +0300

    add routes

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
-
+import 'remixicon/fonts/remixicon.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
---
+++ client/src/pages/SellCar.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+const SellCar = () => {
+    return <div>SellCar</div>
+}
+
+export default SellCar

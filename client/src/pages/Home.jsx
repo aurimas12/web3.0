5664b44274f487c3fac9d749ee8bdaee31af5ec0
aurimas12 <aurimas.butk@gmail.com>
2022-10-25 02:20:10 +0300

    add nft auction page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/ui/HeroSection'
+import LiveAuction from '../components/ui/Live-auction/LiveAuction'
 
 const Home = () => {
   return (
     <>
       <HeroSection/>
+      <LiveAuction/>
     </>
   )
 }

9b83513fe6074fb6b270ac409d7ba26a63d8c48c
aurimas12 <aurimas.butk@gmail.com>
2022-10-28 00:46:28 +0300

    add trending section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from '../components/ui/HeroSection'
 import LiveAuction from '../components/ui/Live-auction/LiveAuction'
 import SellerSelection from '../components/ui/Seller-section/SellerSelection'
+import Trending from '../components/ui/Trending-section/Trending'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <HeroSection/>
       <LiveAuction/>
       <SellerSelection/>
+      <Trending/>
     </>
   )
 }

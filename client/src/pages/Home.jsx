81b759c6ab559eda042f56aab8134ebc63fbd4e2
aurimas12 <aurimas.butk@gmail.com>
2022-10-27 03:18:00 +0300

    add seller section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/ui/HeroSection'
 import LiveAuction from '../components/ui/Live-auction/LiveAuction'
+import SellerSelection from '../components/ui/Seller-section/SellerSelection'
 
 const Home = () => {
   return (
     <>
       <HeroSection/>
       <LiveAuction/>
+      <SellerSelection/>
     </>
   )
 }
